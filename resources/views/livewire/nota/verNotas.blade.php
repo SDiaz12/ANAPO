3b001ec5e33ab269f484845e69f0c6207a94bcc5
MAR2412 <diazfer375@gmail.com>
2025-03-17 00:00:51 -0600

    Cambio

--- resources/views/livewire/nota/verNotas.blade.php
+++ resources/views/livewire/nota/verNotas.blade.php
@@ -19,7 +19,7 @@
                             type="submit" 
                             class="bg-green-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-green-600 transition duration-200 text-sm font-semibold"
                         >
-                            📤 Actualizar Notas
+                            📤 Actualizar Notas Estudiantes
                         </button>
                     </form>
                 </div>
